92b4e2526d09dcb5af1b04f735f88d33a4b2e7bf
2998188620 <2998188620@qq.com>
2022-04-17 01:55:15 +0800

    commit refacted code

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,5 +1,7 @@
 public class Calculator {
-    public int computer(int num1, int num2, char symbol) {
-        return 0;
+
+    public int computer(int num1, int num2, String symbol) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
+        Computable computable = ComputeFactory.getCompute(symbol);
+        return computable.compute(num1, num2);
     }
 }
