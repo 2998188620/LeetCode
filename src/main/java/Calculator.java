407762bed29e09ed6563f44796fcd4919fc8d9f4
2998188620 <2998188620@qq.com>
2022-04-17 03:06:01 +0800

    clear all

--- src/main/java/Calculator.java
+++
@@ -1,6 +0,0 @@
-public class Calculator {
-    public int compute(int num1, int num2, String symbol) throws ClassNotFoundException, InstantiationException, IllegalAccessException {
-        Computable computable = ComputeFactory.getSymbol(symbol);
-        return computable.compute(num1, num2);
-    }
-}
